b3675694e9cf67c6396a2a44152212f69ef66bad
Joe Kracz <joseph.kracz@gmail.com>
2023-09-18 12:03:38 -0700

    updated the content in the about section of the page

--- app/sections/about.section.tsx
+++ app/sections/about.section.tsx
@@ -4,15 +4,22 @@ function AboutSection() {
             <h2 className="font-rubik text-4xl mb-8 text-center">About</h2>
             <p className="text-xl mb-4">Building things that matter - that's what I'm passionate about.</p>
             <p className="text-xl mb-4">
-                With a background in software engineering, I've spent the last 4 years as a product manager, immersed in
-                the fast-paced world of fintech. I've had the privilege of working with innovative teams to create
-                empowering products that educate and equip individuals to better manage their finances.
+                I'm a software engineer by education and have spent the last four years as a product manager in the
+                fintech space. My experience in fast-paced startup environments has sharpened my problem-solving and
+                stakeholder management skills, empowering me to drive impactful outcomes while defining both the "what"
+                and the "why" of the products I build.
+            </p>
+            <p className="text-xl mb-4">
+                My multidisciplinary background equips me to excel not just in coding but also in understanding the
+                broader impact of technology on business and user experience. My experience in leading cross-functional
+                teams and my commitment to agile and engineering best practices allow me to bring ideas from conception
+                to launch efficiently.
             </p>
             <p className="text-xl mb-10">
-                Yet, amidst this fulfilling journey, I found myself missing the unique thrill of 'boots on the ground'
-                problem-solving that's central to engineering. So I decided to dive back into the world of software
-                engineering, eager to apply the strategic lens I've developed as a product manager to solve technical
-                problems.
+                I've been channeling these skills into causecompass.io, an initiative dedicated to bridging the gap
+                between nonprofits and the communities they serve. Through the strategic use of technology and
+                compelling storytelling, we aspire to highlight the unsung heroes and vital initiatives making a
+                real-world impact.
             </p>
         </section>
     );
